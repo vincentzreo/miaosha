0da93aaa4ee1beb4dc488e4ed9c21ca1a9c226e1
zhouzhangqi <1978892854@qq.com>
2019-04-10 14:09:39 +0800

    book模型设计

---
+++ src/main/java/com/zzq/service/model/BookModel.java
@@ -0,0 +1,97 @@
+package com.zzq.service.model;
+
+import java.math.BigDecimal;
+
+public class BookModel {
+    private Integer id;
+    //书名
+    private String title;
+    //作者
+    private String auther;
+    //出版社
+    private String press;
+    //价格
+    private BigDecimal price;
+
+    //库存
+    private Integer stock;
+
+    //描述
+    private String description;
+    //销量
+    private Integer sales;
+    //图片的url
+    private String imgUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuther() {
+        return auther;
+    }
+
+    public void setAuther(String auther) {
+        this.auther = auther;
+    }
+
+    public String getPress() {
+        return press;
+    }
+
+    public void setPress(String press) {
+        this.press = press;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
